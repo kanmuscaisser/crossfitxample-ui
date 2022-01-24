9127e49c58962fb4103270f657c02dbe48ede756
unknown <sebastiangc.2309@gmail.com>
2022-01-24 16:53:27 -0500

    mobile desing for the landing page

--- src/Components/MainPage/Header.jsx
+++ src/Components/MainPage/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { Link } from 'react-router-dom';
 import '../../Styles/MainPage/Header.scss'
 
 const Header = () => {
+    const [slideClick, setSlideClick] = useState(false)
     return <header className="header-Container">
             <p className="title">THE HOUSE OF CROSSFIT</p>
-            <nav>
+            <div className='lineOptions' onClick={()=> setSlideClick(!slideClick)}>
+                        <div className='line'></div>
+                        <div className='line'></div>
+                        <div className='line'></div>
+            </div>
+            <nav className={slideClick? 'slide-Menu' : 'slide-MenuOff'}>
             <ul>
                 <li><Link to='/' >LOG IN</Link></li>
                 <li><Link to='/'>HOME</Link></li>
